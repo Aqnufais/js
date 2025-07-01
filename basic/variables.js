b63cbb87bdc52c4074ee65554841917b44af00ee
aqstin <aqnufais45@gmail.com>
2025-07-01 19:21:24 +0530

    added variables files

--- basic/variables.js
+++ basic/variables.js
@@ -1,1 +1,9 @@
-gi
+const accountId = '12345'
+let accountPassowrd = "1234"
+var emailId = "aqnufais45@me.com"
+
+// accountId = '2345' // cannot be reassigned
+
+// accountPassowrd = '6789' // let allows reassignment
+
+console.table([accountId, accountPassowrd, emailId])
