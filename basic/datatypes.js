27b59b65a46e130728abfa41d37235a880c247cf
aqstin <aqnufais45@gmail.com>
2025-07-02 23:23:58 +0530

    added

---
+++ basic/datatypes.js
@@ -0,0 +1,11 @@
+const dataTypes = {
+    string: "Hello, World!",
+    number: 42,
+    boolean: true,
+    array: [1, 2, 3, 4, 5],
+    object: {
+        key: "value"
+    },
+    nullValue: null,
+    undefinedValue: undefined
+};
